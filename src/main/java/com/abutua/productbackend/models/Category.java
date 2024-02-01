94eb3003ddb5ba4311dcb4ebeeae119686fbf56e
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-02-01 08:07:56 -0300

    first commit

---
+++ src/main/java/com/abutua/productbackend/models/Category.java
@@ -0,0 +1,48 @@
+package com.abutua.productbackend.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TBL_CATEGORY")
+public class Category {
+
+    // Atributos
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    // Métodos Construtores
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+        
+    }
+
+    // Métodos
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
